779e44b4e222122f6da14ea5287a5ba0fdcd712b
Mads Enevoldsen <mads.enevoldsen@jayway.com>
2009-06-26 12:49:05 +0000

    Locale settings changed to SE

--- client/src/main/java/se/streamsource/streamflow/client/MainClient.java
+++ client/src/main/java/se/streamsource/streamflow/client/MainClient.java
@@ -33,7 +33,7 @@ public class MainClient
         System.setProperty("com.apple.mrj.application.apple.menu.about.name", "StreamFlow");
         System.setProperty("apple.laf.useScreenMenuBar", "true");
         System.setProperty("dock:name", "StreamFlow");
-        Locale.setDefault(new Locale("sv", "se", "gov"));
+        Locale.setDefault(new Locale("sv", "SE", "gov"));
 //        Locale.setDefault(Locale.ENGLISH);
 
         org.jdesktop.application.Application.launch(StreamFlowApplication.class, args);
