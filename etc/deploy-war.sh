b4769e0d64ca36db88fb6b5809b88396a7bf4cc9
Ulrik Sandberg <ulrik.sandberg@jayway.com>
2009-08-25 21:49:43 +0000

    Changed etc/deploy-war.sh from just "copy war" to "stop Tomcat", "clean up", "copy war", and "start Tomcat".

--- etc/deploy-war.sh
+++ etc/deploy-war.sh
@@ -7,15 +7,19 @@ if [ $? -ne 0 ]; then
 fi
 base=`basename $war` && 
 
-echo "Retrieving latest war from Nexus repository..."
+echo "Retrieving latest war from Nexus repository: $base"
 if [ ! -f "$base" ]; then
     scp wayuser@192.168.0.146:$war .
 else
     echo "$base already exists"
 fi
 
-echo "Copying to EC2 instance..."
+echo "Copying $base to EC2 instance..."
 scp -i id-eu-streamflow-keypair $base root@`cat dns.txt`:
-echo "Deploying to Tomcat on EC2 instance..."
+echo "Deploying $base to Tomcat on EC2 instance..."
+ssh -i id-eu-streamflow-keypair root@`cat dns.txt` /etc/init.d/tomcat stop
+ssh -i id-eu-streamflow-keypair root@`cat dns.txt` rm -rf /usr/local/tomcat/webapps/streamflow
+ssh -i id-eu-streamflow-keypair root@`cat dns.txt` rm -rf /usr/local/tomcat/work/Catalina/localhost/streamflow
 ssh -i id-eu-streamflow-keypair root@`cat dns.txt` cp $base /usr/local/tomcat/webapps/streamflow.war
+ssh -i id-eu-streamflow-keypair root@`cat dns.txt` /etc/init.d/tomcat start
 ssh -i id-eu-streamflow-keypair root@`cat dns.txt` tail -f /var/log/tomcat/catalina.out
