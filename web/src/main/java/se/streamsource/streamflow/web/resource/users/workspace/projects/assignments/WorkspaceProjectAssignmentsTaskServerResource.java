eb33a6a06833fd1bf4d977a28154e9cc304a0463
Rickard Öberg <rickard.oberg@gmail.com>
2009-09-18 05:13:49 +0000

    Fixed STREAMFLOW-25 (Delegate and forward didn't work in project assignments view)

--- web/src/main/java/se/streamsource/streamflow/web/resource/users/workspace/projects/assignments/WorkspaceProjectAssignmentsTaskServerResource.java
+++ web/src/main/java/se/streamsource/streamflow/web/resource/users/workspace/projects/assignments/WorkspaceProjectAssignmentsTaskServerResource.java
@@ -18,11 +18,15 @@ import org.qi4j.api.unitofwork.UnitOfWork;
 import org.restlet.resource.ResourceException;
 import se.streamsource.streamflow.domain.roles.Describable;
 import se.streamsource.streamflow.resource.roles.StringDTO;
+import se.streamsource.streamflow.resource.roles.EntityReferenceDTO;
 import se.streamsource.streamflow.web.domain.task.Assignee;
 import se.streamsource.streamflow.web.domain.task.Assignments;
 import se.streamsource.streamflow.web.domain.task.Owner;
 import se.streamsource.streamflow.web.domain.task.Task;
 import se.streamsource.streamflow.web.domain.task.TaskEntity;
+import se.streamsource.streamflow.web.domain.task.Delegator;
+import se.streamsource.streamflow.web.domain.task.Delegatee;
+import se.streamsource.streamflow.web.domain.task.Inbox;
 import se.streamsource.streamflow.web.resource.CommandQueryServerResource;
 
 /**
@@ -48,6 +52,33 @@ public class WorkspaceProjectAssignmentsTaskServerResource
         String taskId = (String) getRequest().getAttributes().get("task");
         Describable describable = uowf.currentUnitOfWork().get(Describable.class, taskId);
         describable.describe(stringValue.string().get());
+    }
+
+    public void delegate(EntityReferenceDTO reference)
+    {
+        String userId = (String) getRequest().getAttributes().get("user");
+        String projectId = (String) getRequest().getAttributes().get("project");
+        String taskId = (String) getRequest().getAttributes().get("task");
+
+        UnitOfWork uow = uowf.currentUnitOfWork();
+        Task task = uow.get(Task.class, taskId);
+        Assignments assignments = uowf.currentUnitOfWork().get(Assignments.class, projectId);
+        Delegator delegator = uow.get(Delegator.class, userId);
+        Delegatee delegatee = uow.get(Delegatee.class, reference.entity().get().identity());
+        assignments.delegateAssignedTaskTo(task, delegatee, delegator);
+    }
+
+    public void forward(EntityReferenceDTO reference)
+    {
+        String projectId = (String) getRequest().getAttributes().get("project");
+        String taskId = (String) getRequest().getAttributes().get("task");
+
+        UnitOfWork uow = uowf.currentUnitOfWork();
+        TaskEntity task = uow.get(TaskEntity.class, taskId);
+        Inbox receiverInbox = uow.get(Inbox.class, reference.entity().get().identity());
+
+        Assignments assignments = uowf.currentUnitOfWork().get(Assignments.class, projectId);
+        assignments.forwardAssignedTask(task, receiverInbox);
     }
 
     public void markAsRead()
