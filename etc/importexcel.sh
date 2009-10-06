6473de0df1e187d3856ca1fc777f22a486988e54
Rickard Öberg <rickard.oberg@gmail.com>
2009-10-06 08:49:10 +0000

    Added ExcelBundle jar

--- etc/importexcel.sh
+++ etc/importexcel.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar ../client/src/main/lib/excelbundle.jar -p -r ../client/src/main/resources/ -import ../client/target/streamflow.xls -l default,sv_SE_gov -ref sv
+java -jar ../client/src/main/lib/excelbundle.jar -p -r ../client/src/main/resources/ -import ../client/target/streamflow.xls -l sv_SE_gov -ref sv
