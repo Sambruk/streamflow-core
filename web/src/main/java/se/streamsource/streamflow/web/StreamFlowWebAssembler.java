7b580a6b4518b1c0a25e76a4dcb0db0247c2c0a8
Rickard Öberg <rickard.oberg@gmail.com>
2009-10-08 06:16:43 +0000

    Changed version

--- web/src/main/java/se/streamsource/streamflow/web/StreamFlowWebAssembler.java
+++ web/src/main/java/se/streamsource/streamflow/web/StreamFlowWebAssembler.java
@@ -52,7 +52,7 @@ public class StreamFlowWebAssembler
     {
         ApplicationAssembly assembly = applicationFactory.newApplicationAssembly();
         assembly.setName("StreamFlow web");
-        assembly.setVersion("0.1.14.357");
+        assembly.setVersion("0.1.15.0");
         LayerAssembly configurationLayer = assembly.layerAssembly("Configuration");
         LayerAssembly domainInfrastructureLayer = assembly.layerAssembly("Domain infrastructure");
         LayerAssembly domainLayer = assembly.layerAssembly("Domain");
