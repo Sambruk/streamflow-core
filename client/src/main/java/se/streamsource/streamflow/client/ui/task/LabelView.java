4b72fc9a3570733ba8d37d81fa82716c707f0a73
Arvid Huss <arvid.huss@jayway.com>
2009-11-18 16:20:04 +0000

    72 - Move selection of labels to 'General Tab' in 'Task Detail'.

--- client/src/main/java/se/streamsource/streamflow/client/ui/task/LabelView.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/task/LabelView.java
@@ -8,9 +8,10 @@ import se.streamsource.streamflow.infrastructure.application.ListItemValue;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class LabelView extends JPanel
+    implements FocusListener, KeyListener
 {
 	ListItemValue itemValue;
 	JButton button;
@@ -19,13 +20,21 @@ public class LabelView extends JPanel
 	{
 		super(new FlowLayout(FlowLayout.LEFT,2,1));
 		this.itemValue = itemValue;
+
+        setFocusable(true);
+        this.setRequestFocusEnabled(true);
+        
 		JXLabel label = new JXLabel(itemValue.description().get());
 		button = new JButton(i18n.icon(Icons.deleteLabel, 16));
 		button.setBorder(new EmptyBorder(new Insets(0,0,0,0)));
-
+        button.setFocusable(false);
+        
 		this.add(label);
 		this.add(button);
         setBorder(BorderFactory.createEtchedBorder());
+
+        addFocusListener(this);
+        addKeyListener(this);
         
 	}
 
@@ -38,4 +47,31 @@ public class LabelView extends JPanel
 	{
 		button.addActionListener(listener);
 	}
+
+    public void focusGained(FocusEvent e) {
+        setBorder(BorderFactory.createEtchedBorder(Color.LIGHT_GRAY ,Color.BLUE));
+        repaint();
+    }
+
+    public void focusLost(FocusEvent e) {
+        setBorder(BorderFactory.createEtchedBorder());
+        repaint();
+    }
+
+    public void keyTyped(KeyEvent e)
+    {
+    }
+
+    public void keyPressed(KeyEvent e)
+    {
+        if(e.getKeyChar() == KeyEvent.VK_BACK_SPACE
+                || e.getKeyChar() == KeyEvent.VK_DELETE)
+        {
+            button.doClick();
+        }
+    }
+
+    public void keyReleased(KeyEvent e)
+    {
+    }
 }
