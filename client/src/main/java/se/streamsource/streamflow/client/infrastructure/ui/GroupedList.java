dfb17ebc9ff9060b56a3168568eb8170af532976
Henrik Bernström <henrik.bernstrom@jayway.com>
2010-02-03 09:01:40 +0000

    Added GroupedList.

---
+++ client/src/main/java/se/streamsource/streamflow/client/infrastructure/ui/GroupedList.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2009, Rickard Öberg. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package se.streamsource.streamflow.client.infrastructure.ui;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import se.streamsource.streamflow.infrastructure.application.ListItemValue;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.SeparatorList;
+import ca.odell.glazedlists.SortedList;
+import ca.odell.glazedlists.swing.EventListModel;
+
+/**
+ * JAVADOC
+ */
+public class GroupedList
+      extends JPanel
+{
+   private JList list;
+   private EventListModel listModel;
+   public JScrollPane pane = new JScrollPane();
+
+   public GroupedList()
+   {
+      setLayout( new BorderLayout() );
+
+      list = new JList();
+      list.setCellRenderer( new SeparatorListCellRenderer(new ListItemListCellRenderer()) );
+      list.getSelectionModel().addListSelectionListener( new ListSelectionListener()
+      {
+         public void valueChanged( ListSelectionEvent e )
+         {
+            if (list.getSelectedValue() instanceof SeparatorList.Separator)
+               list.clearSelection();
+         }
+      });
+      pane.setViewportView( list );
+
+      add( pane, BorderLayout.CENTER );
+   }
+
+   public JList getList()
+   {
+      return list;
+   }
+
+   public JScrollPane getPane()
+   {
+      return pane;
+   }
+
+   public void setEventList( EventList<ListItemValue> eventList )
+   {
+      SortedList<ListItemValue> sortedIssues = new SortedList<ListItemValue>( eventList, new ListItemComparator() );
+
+      listModel = new EventListModel<ListItemValue>( new SeparatorList<ListItemValue>( sortedIssues, new ListItemGroupingComparator(), 1, 10000 ) );
+
+      list.setModel( listModel );
+   }
+}
