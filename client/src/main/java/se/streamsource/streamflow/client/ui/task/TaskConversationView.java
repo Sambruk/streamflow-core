a4f7ad8726a6c565b459d1709c431c16cba9271c
Henrik Bernström <henrik.bernstrom@jayway.com>
2010-02-24 16:27:35 +0000

    D3.3 - Improved messages displayal.

--- client/src/main/java/se/streamsource/streamflow/client/ui/task/TaskConversationView.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/task/TaskConversationView.java
@@ -14,21 +14,11 @@
 
 package se.streamsource.streamflow.client.ui.task;
 
-import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.event.ListEvent;
-import ca.odell.glazedlists.event.ListEventListener;
-import org.jdesktop.application.Action;
-import org.jdesktop.application.ApplicationContext;
-import org.qi4j.api.injection.scope.Service;
-import org.qi4j.api.injection.scope.Structure;
-import org.qi4j.api.object.ObjectBuilderFactory;
-import se.streamsource.streamflow.client.MacOsUIExtension;
-import se.streamsource.streamflow.client.infrastructure.ui.RefreshWhenVisible;
-import se.streamsource.streamflow.client.infrastructure.ui.StateBinder;
-import se.streamsource.streamflow.client.infrastructure.ui.i18n;
-import se.streamsource.streamflow.client.ui.workspace.WorkspaceResources;
-import se.streamsource.streamflow.infrastructure.application.LinkValue;
-import se.streamsource.streamflow.resource.conversation.MessageDTO;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.text.SimpleDateFormat;
 
 import javax.swing.ActionMap;
 import javax.swing.JButton;
@@ -37,15 +27,24 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.text.html.HTMLEditorKit;
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.text.SimpleDateFormat;
 
-public class TaskConversationView
-      extends JPanel
-      implements ListEventListener
+import org.jdesktop.application.Action;
+import org.jdesktop.application.ApplicationContext;
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.injection.scope.Structure;
+import org.qi4j.api.object.ObjectBuilderFactory;
+
+import se.streamsource.streamflow.client.MacOsUIExtension;
+import se.streamsource.streamflow.client.infrastructure.ui.RefreshWhenVisible;
+import se.streamsource.streamflow.client.infrastructure.ui.StateBinder;
+import se.streamsource.streamflow.client.infrastructure.ui.i18n;
+import se.streamsource.streamflow.client.ui.workspace.WorkspaceResources;
+import se.streamsource.streamflow.resource.conversation.MessageDTO;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.event.ListEvent;
+import ca.odell.glazedlists.event.ListEventListener;
+
+public class TaskConversationView extends JPanel implements ListEventListener
 {
 
    private ObjectBuilderFactory obf;
@@ -63,61 +62,63 @@ public class TaskConversationView
    private JTextField defaultFocusField;
    private JPanel sendPanel;
 
-   public TaskConversationView( @Service ApplicationContext context, @Structure ObjectBuilderFactory obf )
+   public TaskConversationView(@Service ApplicationContext context,
+         @Structure ObjectBuilderFactory obf)
    {
-      super( new BorderLayout() );
+      super(new BorderLayout());
       this.context = context;
       this.obf = obf;
-      am = context.getActionMap( this );
-      MacOsUIExtension.convertAccelerators( am );
+      am = context.getActionMap(this);
+      MacOsUIExtension.convertAccelerators(am);
 
       messages = new JTextPane();
-      messages.setContentType( "text/html" );
-      ((HTMLEditorKit) messages.getEditorKit()).setAutoFormSubmission( false );
-      messages.setEditable( false );
+      messages.setContentType("text/html");
+      ((HTMLEditorKit) messages.getEditorKit()).setAutoFormSubmission(false);
+      messages.setEditable(false);
 
       JScrollPane scroll = new JScrollPane();
-      scroll.getViewport().add( messages );
-      add( scroll, BorderLayout.CENTER );
+      scroll.getViewport().add(messages);
+      add(scroll, BorderLayout.CENTER);
 
       add(initBottom(), BorderLayout.SOUTH);
 
-      refresher = new RefreshWhenVisible( this );
-      addAncestorListener( refresher );
+      refresher = new RefreshWhenVisible(this);
+      addAncestorListener(refresher);
    }
 
    private JPanel initBottom()
    {
-      bottomPanel = new JPanel( new CardLayout() );
+      bottomPanel = new JPanel(new CardLayout());
 
       // INITIAL
-      JPanel createPanel = new JPanel( new FlowLayout( FlowLayout.RIGHT ) );
-      createPanel.setPreferredSize( new Dimension(100, 40) );
+      JPanel createPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+      createPanel.setPreferredSize(new Dimension(100, 40));
       // with a button for add message and add participant
-      JButton newMsg = new JButton( am.get( "showNewMessage" ) );
-      createPanel.add( newMsg );
+      JButton newMsg = new JButton(am.get("showNewMessage"));
+      createPanel.add(newMsg);
 
       // NEWMESSAGE
       sendPanel = new JPanel(new BorderLayout());
-      sendPanel.setPreferredSize( new Dimension( 100, 100) );
+      sendPanel.setPreferredSize(new Dimension(100, 100));
       JScrollPane messageScroll = new JScrollPane();
-      newMessage = new JTextPane( );
-      newMessage.setEditable( true );
-      messageScroll.getViewport().add( newMessage );
+      newMessage = new JTextPane();
+      newMessage.setContentType("text/html");
+      newMessage.setEditable(true);
+      messageScroll.getViewport().add(newMessage);
 
       JPanel sendMessagePanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-      JButton addMessage = new JButton( am.get( "addMessage" ) );
-      JButton cancel = new JButton( am.get( "cancel" ) );
-      sendMessagePanel.add( addMessage );
-      sendMessagePanel.add( cancel );
+      JButton addMessage = new JButton(am.get("addMessage"));
+      JButton cancel = new JButton(am.get("cancel"));
+      sendMessagePanel.add(addMessage);
+      sendMessagePanel.add(cancel);
 
-      sendPanel.add( messageScroll, BorderLayout.CENTER );
-      sendPanel.add( sendMessagePanel, BorderLayout.SOUTH );
+      sendPanel.add(messageScroll, BorderLayout.CENTER);
+      sendPanel.add(sendMessagePanel, BorderLayout.SOUTH);
 
-      bottomPanel.add( createPanel, "INITIAL" );
-      //bottomPanel.add( sendPanel, "NEW_MESSAGE" );
+      bottomPanel.add(createPanel, "INITIAL");
+      // bottomPanel.add( sendPanel, "NEW_MESSAGE" );
 
-      ((CardLayout) bottomPanel.getLayout()).show( bottomPanel, "INITIAL" );
+      ((CardLayout) bottomPanel.getLayout()).show(bottomPanel, "INITIAL");
 
       return bottomPanel;
    }
@@ -125,98 +126,109 @@ public class TaskConversationView
    @Action
    public void showNewMessage()
    {
-      bottomPanel.add( sendPanel, "NEW_MESSAGE" );
-      ((CardLayout) bottomPanel.getLayout()).show( bottomPanel, "NEW_MESSAGE" );
+      bottomPanel.add(sendPanel, "NEW_MESSAGE");
+      ((CardLayout) bottomPanel.getLayout()).show(bottomPanel, "NEW_MESSAGE");
    }
 
    @Action
    public void addMessage()
    {
-      model.addMessage( newMessage.getText() );
-      bottomPanel.remove( sendPanel );
-      ((CardLayout) bottomPanel.getLayout()).show( bottomPanel, "INITIAL" );
+      model.addMessage(newMessage.getText());
+      bottomPanel.remove(sendPanel);
+      ((CardLayout) bottomPanel.getLayout()).show(bottomPanel, "INITIAL");
+      newMessage.setText(null);
    }
 
    @Action
    public void cancel()
    {
-      bottomPanel.remove( sendPanel );
-      ((CardLayout) bottomPanel.getLayout()).show( bottomPanel, "INITIAL" );
+      bottomPanel.remove(sendPanel);
+      ((CardLayout) bottomPanel.getLayout()).show(bottomPanel, "INITIAL");
    }
 
    @Action
    public void showParticipants()
    {
-      participantView.setModel( model.getParticipantsModel() );
-      ((CardLayout) bottomPanel.getLayout()).show( bottomPanel, "NEW_PARTICIPANT" );
+      participantView.setModel(model.getParticipantsModel());
+      ((CardLayout) bottomPanel.getLayout()).show(bottomPanel,
+            "NEW_PARTICIPANT");
    }
 
    @Action
    public void participants()
    {
+      // buf.append( "<strong>" + model.getDescription() + "</strong>" );
+      // buf.append( "  ( " );
+      // for(Object participant : model.participants() )
+      // {
+      // buf.append( ((LinkValue)participant).text() + " " );
+      // }
+      // buf.append( ")<br/>" );
+      
 
    }
 
-   public void setModel( TaskConversationModel taskConversationDetailModel )
+   public void setModel(TaskConversationModel taskConversationDetailModel)
    {
       if (model != null)
-         model.messages().removeListEventListener( this );
+         model.messages().removeListEventListener(this);
 
       model = taskConversationDetailModel;
       model.refresh();
-      refresher.setRefreshable( model );
+      refresher.setRefreshable(model);
 
       if (model != null)
       {
-         taskConversationDetailModel.messages().addListEventListener( this );
+         taskConversationDetailModel.messages().addListEventListener(this);
 
-         model.messages().addListEventListener( this );
-         listChanged( null );
+         model.messages().addListEventListener(this);
+         listChanged(null);
       }
 
    }
 
-   public void listChanged( ListEvent listEvent )
+   public void listChanged(ListEvent listEvent)
    {
-
       EventList<MessageDTO> list = model.messages();
       StringBuffer buf = new StringBuffer();
 
-      buf.append( "<strong>" + model.getDescription() + "</strong>" );
-      buf.append( "  ( " );
-      for(Object participant : model.participants() )
-      {
-         buf.append( ((LinkValue)participant).text() + " " );  
-      }
-      buf.append( ")<br/>" );
+      buf.append( "<html><head></head><body>");
 
-      buf.append( "<table border='NONE' cellpadding='10'" );
       int size = list.size();
-      for (int i = 0; i < size; i++)
+      if (size > 0)
       {
-         MessageDTO messageDTO = list.get( i );
+         buf.append("<table border='NONE' cellpadding='10'>");
+         for (int i = 0; i < size; i++)
+         {
+            MessageDTO messageDTO = list.get(i);
 
-         buf.append( "<tr>" );
-         buf.append( "<td width='150' align='left' valign='top'>" );
-         buf.append( messageDTO.sender().get() );
-         buf.append( "<br>" );
-         buf.append( new SimpleDateFormat( i18n.text( WorkspaceResources.date_time_format ) ).format( messageDTO.createdOn().get() ) );
-         buf.append( "</td><td width='" + getMessageTableLastColSize() + "' style='WORD-BREAK:BREAK-ALL'>" );
-         buf.append( messageDTO.text().get() );
-         buf.append( "<hr width='100%' style='border:1px solid #cccccc; padding-top: 15px;'>" );
-         buf.append( "</td>" );
-         buf.append( "</tr>" );
+            buf.append("<tr>");
+            buf.append("<td width='150' align='left' valign='top'>");
+            buf.append("<p>");
+            buf.append(messageDTO.sender().get());
+            buf.append("</p><p>");
+            buf.append(new SimpleDateFormat(i18n
+                  .text(WorkspaceResources.date_time_format)).format(messageDTO
+                  .createdOn().get()));
+            buf.append("</p></td><td width='" + getMessageTableLastColSize()
+                  + "' style=''>");
+            buf.append(messageDTO.text().get());
+            buf
+                  .append("<hr width='100%' style='border:1px solid #cccccc; padding-top: 15px;'>");
+            buf.append("</td>");
+            buf.append("</tr>");
 
-
+         }
+         buf.append("</table>");
       }
-      buf.append( "</table>" );
-      messages.setText( buf.toString() );
-
+      buf.append( "</body></html>");
+      messages.setText(buf.toString());
    }
 
    private int getMessageTableLastColSize()
    {
-      return (int) (messages.getVisibleRect().getWidth() < 600 ? 450 : (messages.getVisibleRect().getWidth() - 150));
+      return (int) (messages.getVisibleRect().getWidth() < 600 ? 450
+            : (messages.getVisibleRect().getWidth() - 150));
    }
 
 }
