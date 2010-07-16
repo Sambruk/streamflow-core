86d067a7980dd8304d55227d4e282138150342fd
Arvid Huss <arvid.huss@jayway.com>
2010-07-16 07:03:42 +0000

    STREAMFLOW-278 Fix workspace tree anchorage Removed changes due to hanging graphics

--- client/src/main/java/se/streamsource/streamflow/client/ui/workspace/WorkspaceView.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/workspace/WorkspaceView.java
@@ -75,8 +75,6 @@ import java.awt.Font;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 /**
  * JAVADOC
@@ -378,21 +376,6 @@ public class WorkspaceView
 
          public void treeWillCollapse( TreeExpansionEvent event ) throws ExpandVetoException
          {
-         }
-      } );
-
-      workspaceTree.addMouseListener( new MouseAdapter()
-      {
-
-         @Override
-         public void mouseExited( MouseEvent e )
-         {
-            super.mouseExited( e );
-            if (popup != null)
-            {
-               popup.hide();
-               popup = null;
-            }
          }
       } );
    }
