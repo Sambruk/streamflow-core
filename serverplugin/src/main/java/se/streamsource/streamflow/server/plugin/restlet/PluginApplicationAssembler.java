6bbdb0f9d1cf94e68a265b238cb65f0c5dbd2fd5
Arvid Huss <arvid.huss@jayway.com>
2010-09-06 11:18:33 +0000

    Release preparation

--- serverplugin/src/main/java/se/streamsource/streamflow/server/plugin/restlet/PluginApplicationAssembler.java
+++ serverplugin/src/main/java/se/streamsource/streamflow/server/plugin/restlet/PluginApplicationAssembler.java
@@ -1,15 +1,18 @@
-/*
- * Copyright (c) 2010, Rickard Öberg. All Rights Reserved.
+/**
+ *
+ * Copyright 2009-2010 Streamsource AB
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
 package se.streamsource.streamflow.server.plugin.restlet;
@@ -32,7 +35,7 @@ import se.streamsource.streamflow.server.plugin.contact.ContactValue;
  * Assembler for the plugin application
  */
 public class PluginApplicationAssembler
-   implements ApplicationAssembler
+      implements ApplicationAssembler
 {
    Assembler pluginAssembler;
 
@@ -50,7 +53,7 @@ public class PluginApplicationAssembler
       ModuleAssembly rest = webLayer.moduleAssembly( "REST" );
 
       // Plugin wrappers
-      rest.addObjects( ContactLookupRestlet.class);
+      rest.addObjects( ContactLookupRestlet.class );
 
       // Plugins goes here
       LayerAssembly pluginLayer = app.layerAssembly( "Plugins" );
@@ -61,7 +64,7 @@ public class PluginApplicationAssembler
             ContactValue.class,
             ContactAddressValue.class,
             ContactEmailValue.class,
-            ContactPhoneValue.class).visibleIn( Visibility.application );
+            ContactPhoneValue.class ).visibleIn( Visibility.application );
 
       pluginAssembler.assemble( moduleAssembly );
 
