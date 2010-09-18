3107173cb7d07eed80f7422d80567195e5f43ae6
Arvid Huss <arvid.huss@jayway.com>
2010-09-18 15:06:14 +0000

    Final fixes for release 1.1

--- web/src/main/java/se/streamsource/streamflow/web/context/surface/accesspoints/endusers/formdrafts/summary/UpdateCaseCountFormSummaryConcern.java
+++ web/src/main/java/se/streamsource/streamflow/web/context/surface/accesspoints/endusers/formdrafts/summary/UpdateCaseCountFormSummaryConcern.java
@@ -1,8 +1,13 @@
-/*
- * Copyright (c) 2010, Mads Enevoldsen. All Rights Reserved.
+/**
+ *
+ * Copyright 2009-2010 Streamsource AB
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,7 +23,6 @@ import org.qi4j.api.injection.scope.Service;
 import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.unitofwork.UnitOfWorkFactory;
 import se.streamsource.dci.api.RoleMap;
-import se.streamsource.streamflow.web.context.surface.accesspoints.endusers.CaseContext;
 import se.streamsource.streamflow.web.domain.entity.caze.CaseEntity;
 import se.streamsource.streamflow.web.infrastructure.caching.Caches;
 import se.streamsource.streamflow.web.infrastructure.caching.Caching;
@@ -28,8 +32,8 @@ import se.streamsource.streamflow.web.infrastructure.caching.CachingService;
  * Update case counts when submitted from Surface
  */
 public abstract class UpdateCaseCountFormSummaryConcern
-   extends ConcernOf<SummaryContext>
-   implements SummaryContext
+      extends ConcernOf<SummaryContext>
+      implements SummaryContext
 {
    @Optional
    @Service
@@ -46,6 +50,6 @@ public abstract class UpdateCaseCountFormSummaryConcern
       CaseEntity caze = roleMap.get( CaseEntity.class );
 
       // Update project inbox cache
-      new Caching(caching, Caches.CASECOUNTS).addToCache( caze.owner().get().toString(), 1 );
+      new Caching( caching, Caches.CASECOUNTS ).addToCache( caze.owner().get().toString(), 1 );
    }
 }
