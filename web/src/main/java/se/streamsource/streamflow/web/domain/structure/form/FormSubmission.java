3a94ed43409c49cd5518c94a4d3df78cab19fb41
Mads Enevoldsen <mads.enevoldsen@jayway.com>
2010-09-20 08:48:07 +0000

    STREAMFLOW-347 FormSubmission also does a regexp validation of the string (if enabled). Otherwise Surface would accept every value.

--- web/src/main/java/se/streamsource/streamflow/web/domain/structure/form/FormSubmission.java
+++ web/src/main/java/se/streamsource/streamflow/web/domain/structure/form/FormSubmission.java
@@ -40,7 +40,11 @@ import se.streamsource.streamflow.domain.form.TextFieldValue;
 import se.streamsource.streamflow.infrastructure.event.DomainEvent;
 import se.streamsource.streamflow.util.Strings;
 
+import javax.swing.text.JTextComponent;
+import java.text.ParseException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * JAVADOC
@@ -207,6 +211,18 @@ public interface FormSubmission
 
       private boolean validate( TextFieldValue definition, String value )
       {
+         if (Strings.notEmpty( definition.regularExpression().get() ))
+         {
+            if ( value != null )
+            {
+               Pattern pattern = Pattern.compile( definition.regularExpression().get() );
+               Matcher matcher = pattern.matcher( value );
+
+               return matcher.matches();
+            }
+            return false;
+         }
+
          return value != null;
 
       }
