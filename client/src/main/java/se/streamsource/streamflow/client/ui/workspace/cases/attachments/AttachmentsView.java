bfad6b7562af9998995d94d0a9eed342c36377cb
Rickard Öberg <rickard.oberg@gmail.com>
2010-11-03 08:22:44 +0000

    Fixed removal of attachment

--- client/src/main/java/se/streamsource/streamflow/client/ui/workspace/cases/attachments/AttachmentsView.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/workspace/cases/attachments/AttachmentsView.java
@@ -31,39 +31,27 @@ import org.qi4j.api.object.ObjectBuilderFactory;
 import org.restlet.engine.io.BioUtils;
 import se.streamsource.dci.restlet.client.CommandQueryClient;
 import se.streamsource.streamflow.client.StreamflowResources;
-import se.streamsource.streamflow.client.util.dialog.DialogService;
+import se.streamsource.streamflow.client.ui.workspace.WorkspaceResources;
+import se.streamsource.streamflow.client.util.CommandTask;
 import se.streamsource.streamflow.client.util.RefreshWhenVisible;
 import se.streamsource.streamflow.client.util.SelectionActionEnabler;
 import se.streamsource.streamflow.client.util.dialog.ConfirmationDialog;
+import se.streamsource.streamflow.client.util.dialog.DialogService;
 import se.streamsource.streamflow.client.util.i18n;
-import se.streamsource.streamflow.client.ui.workspace.WorkspaceResources;
-import se.streamsource.streamflow.client.util.CommandTask;
 import se.streamsource.streamflow.domain.attachment.AttachmentValue;
 import se.streamsource.streamflow.infrastructure.event.TransactionEvents;
 import se.streamsource.streamflow.infrastructure.event.source.TransactionListener;
 import se.streamsource.streamflow.infrastructure.event.source.helper.Events;
 
-import javax.swing.ActionMap;
-import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.KeyStroke;
-import javax.swing.ListSelectionModel;
-import java.awt.BorderLayout;
-import java.awt.Desktop;
-import java.awt.KeyboardFocusManager;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * JAVADOC
@@ -175,16 +163,27 @@ public class AttachmentsView
 
       if (dialog.isConfirmed())
       {
+         final List<AttachmentValue> removedAttachments = new ArrayList<AttachmentValue>( );
+         for (int i : attachments.getSelectedRows())
+         {
+            removedAttachments.add(attachmentsModel.getEventList().get( attachments.convertRowIndexToModel( i ) ));
+         }
+
          return new CommandTask()
          {
             @Override
             public void command()
                   throws Exception
             {
-               for (int i : attachments.getSelectedRows())
+               try
                {
-                  AttachmentValue attachment = attachmentsModel.getEventList().get( attachments.convertRowIndexToModel( i ) );
-                  attachmentsModel.removeAttachment( attachment );
+                  for (AttachmentValue removedAttachment : removedAttachments)
+                  {
+                     attachmentsModel.removeAttachment( removedAttachment );
+                  }
+               } catch (Throwable e)
+               {
+                  e.printStackTrace();
                }
             }
          };
