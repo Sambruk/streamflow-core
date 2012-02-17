173839c621d4e5fe2294d2a3e04cd8e7863276d9
Arvid Huss <arvid.huss@jayway.com>
2012-02-17 08:45:53 +0100

    STREAMFLOW-620 Formulär fält namn och beskrivning fixar inte svenska bokstäver

--- client/src/main/java/se/streamsource/streamflow/client/ui/administration/forms/definition/FieldValueEditModel.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/administration/forms/definition/FieldValueEditModel.java
@@ -16,23 +16,21 @@
  */
 package se.streamsource.streamflow.client.ui.administration.forms.definition;
 
+import ca.odell.glazedlists.BasicEventList;
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.TransactionList;
 import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.injection.scope.Uses;
 import org.qi4j.api.structure.Module;
 import org.qi4j.api.value.ValueBuilder;
 import org.restlet.data.Form;
 import org.restlet.resource.ResourceException;
-
 import se.streamsource.dci.restlet.client.CommandQueryClient;
 import se.streamsource.dci.value.EntityValue;
 import se.streamsource.dci.value.link.LinkValue;
 import se.streamsource.dci.value.link.LinksValue;
 import se.streamsource.streamflow.api.administration.form.FieldDefinitionAdminValue;
-import se.streamsource.streamflow.api.administration.form.FieldDefinitionValue;
 import se.streamsource.streamflow.client.util.Refreshable;
-import ca.odell.glazedlists.BasicEventList;
-import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.TransactionList;
 
 /**
  * JAVADOC
@@ -75,69 +73,95 @@ public class FieldValueEditModel
 
    public void changeMandatory( boolean mandatory ) throws ResourceException
    {
-      client.postCommand( "changemandatory", new Form("mandatory="+mandatory).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "mandatory", ""+mandatory );
+      client.postCommand( "changemandatory", form.getWebRepresentation() );
    }
 
    public void changeDescription( String newDescription ) throws ResourceException
    {
-      client.postCommand( "changedescription", new Form("description="+newDescription).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "description", newDescription );
+      client.postCommand( "changedescription", form.getWebRepresentation() );
    }
 
    public void changeNote( String newNote ) throws ResourceException
    {
-      client.postCommand( "changenote", new Form("string="+newNote).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "string", newNote );
+      client.postCommand( "changenote", form.getWebRepresentation() );
    }
 
    public void changeFieldId( String newId ) throws ResourceException
    {
-      client.postCommand( "changefieldid", new Form("id="+newId).getWebRepresentation() );
+      Form form = new Form(  );
+      form.set( "id", newId );
+      client.postCommand( "changefieldid", form.getWebRepresentation() );
    }
 
    public void changeWidth( Integer newWidth ) throws ResourceException
    {
-      client.postCommand( "changewidth", new Form("width="+newWidth).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "width", newWidth.toString() );
+      client.postCommand( "changewidth", form.getWebRepresentation() );
    }
 
    public void changeRows( Integer newRows ) throws ResourceException
    {
-      client.postCommand( "changerows", new Form("rows="+newRows).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "rows", newRows.toString() );
+      client.postCommand( "changerows", form.getWebRepresentation() );
    }
 
    public void changeCols( Integer newColumns ) throws ResourceException
    {
-      client.postCommand( "changecols", new Form("columns="+newColumns).getWebRepresentation() );
+      Form form = new Form(  );
+      form.set( "columns", newColumns.toString() );
+      client.postCommand( "changecols", form.getWebRepresentation() );
    }
 
    public void changeInteger( Boolean isInteger ) throws ResourceException
    {
-      client.postCommand( "changeinteger", new Form("integer="+isInteger).getWebRepresentation() );
+      Form form = new Form(  );
+      form.set( "integer", isInteger.toString() );
+      client.postCommand( "changeinteger", form.getWebRepresentation() );
    }
 
    public void changeComment( String comment ) throws ResourceException
    {
-      client.postCommand( "changecomment", new Form("comment="+comment).getWebRepresentation() );
+      Form form = new Form(  );
+      form.set( "comment", comment );
+      client.postCommand( "changecomment", form.getWebRepresentation() );
    }
 
    public void changeHint( String hint ) throws ResourceException
    {
-      client.postCommand( "changehint", new Form("hint="+hint).getWebRepresentation() );
+      Form form = new Form(  );
+      form.set( "hint", hint );
+      client.postCommand( "changehint", form.getWebRepresentation() );
    }
 
    public void changeRegularExpression( String regularExpression ) throws ResourceException
    {
-      client.postCommand( "changeregularexpression", new Form("expression="+regularExpression).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "expression", regularExpression );
+      client.postCommand( "changeregularexpression", form.getWebRepresentation() );
    }
 
    public void changeOpenSelectionName( String name )
    {
-      client.postCommand( "changeopenselectionname", new Form("name="+name).getWebRepresentation() );
+      Form form = new Form( );
+      form.set( "name", name );
+      client.postCommand( "changeopenselectionname", form.getWebRepresentation() );
    }
 
    public void changeDatatype( String id ) throws ResourceException
    {
       if (id != null)
       {
-         client.postCommand( "changedatatype", new Form("entity="+id).getWebRepresentation() );
+         Form form = new Form( );
+         form.set( "entity", id );
+         client.postCommand( "changedatatype", form.getWebRepresentation() );
       } else
       {
          client.postCommand( "changedatatype", module.valueBuilderFactory().newValue(EntityValue.class) );
