591bb9ac6e1f9c152c417e6a9010cb68ad592863
Arvid Huss <arvid.huss@jayway.com>
2012-08-24 15:27:49 +0200

    STREAMFLOW-692 Anpassa Streamflow SPI för import av användare och grupper från LDAP Fixed issues with action enabling functionality for option actions on groups, group and organization users.

--- client/src/main/java/se/streamsource/streamflow/client/ui/administration/casetypes/CaseTypesView.java
+++ client/src/main/java/se/streamsource/streamflow/client/ui/administration/casetypes/CaseTypesView.java
@@ -16,18 +16,8 @@
  */
 package se.streamsource.streamflow.client.ui.administration.casetypes;
 
-import static se.streamsource.streamflow.client.util.i18n.text;
-
-import java.awt.Component;
-import java.awt.Desktop;
-import java.awt.Dimension;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-
-import javax.swing.ActionMap;
-import javax.swing.JList;
-
+import ca.odell.glazedlists.EventList;
+import ca.odell.glazedlists.swing.EventListModel;
 import org.jdesktop.application.Action;
 import org.jdesktop.application.ApplicationContext;
 import org.jdesktop.application.Task;
@@ -35,7 +25,6 @@ import org.qi4j.api.injection.scope.Service;
 import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.injection.scope.Uses;
 import org.qi4j.api.structure.Module;
-
 import se.streamsource.dci.value.ResourceValue;
 import se.streamsource.dci.value.link.LinkValue;
 import se.streamsource.streamflow.client.StreamflowResources;
@@ -46,15 +35,24 @@ import se.streamsource.streamflow.client.util.ListDetailView;
 import se.streamsource.streamflow.client.util.RefreshWhenShowing;
 import se.streamsource.streamflow.client.util.ResourceActionEnabler;
 import se.streamsource.streamflow.client.util.TabbedResourceView;
-import se.streamsource.streamflow.client.util.i18n;
 import se.streamsource.streamflow.client.util.dialog.ConfirmationDialog;
 import se.streamsource.streamflow.client.util.dialog.DialogService;
 import se.streamsource.streamflow.client.util.dialog.NameDialog;
 import se.streamsource.streamflow.client.util.dialog.SelectLinkDialog;
+import se.streamsource.streamflow.client.util.i18n;
 import se.streamsource.streamflow.infrastructure.event.domain.TransactionDomainEvents;
 import se.streamsource.streamflow.util.Strings;
-import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.swing.EventListModel;
+
+import javax.swing.ActionMap;
+import javax.swing.JList;
+import java.awt.Component;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static se.streamsource.streamflow.client.util.i18n.*;
 
 /**
  * JAVADOC
