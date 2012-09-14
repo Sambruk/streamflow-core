2aff90c99accf29956eb7a3f01bc80f667c69f1c
Arvid Huss <arvid.huss@jayway.com>
2012-09-14 15:04:34 +0200

    Increased logging for backup/restore/reindex

--- web/src/main/java/se/streamsource/streamflow/web/management/ManagerComposite.java
+++ web/src/main/java/se/streamsource/streamflow/web/management/ManagerComposite.java
@@ -16,25 +16,9 @@
  */
 package se.streamsource.streamflow.web.management;
 
-import static org.qi4j.api.util.Iterables.count;
-import static org.qi4j.api.util.Iterables.filter;
-import static se.streamsource.streamflow.infrastructure.event.domain.source.helper.Events.events;
-import static se.streamsource.streamflow.infrastructure.event.domain.source.helper.Events.withNames;
-
-import java.io.File;
-import java.io.FileFilter;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.zip.GZIPOutputStream;
-
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
+import org.joda.time.format.PeriodFormat;
 import org.openrdf.repository.Repository;
 import org.qi4j.api.Qi4j;
 import org.qi4j.api.composite.Composite;
@@ -64,7 +48,6 @@ import org.qi4j.spi.query.EntityFinder;
 import org.qi4j.spi.structure.ModuleSPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import se.streamsource.streamflow.infrastructure.configuration.FileConfiguration;
 import se.streamsource.streamflow.infrastructure.event.domain.TransactionDomainEvents;
 import se.streamsource.streamflow.infrastructure.event.domain.factory.DomainEventFactory;
@@ -84,6 +67,23 @@ import se.streamsource.streamflow.web.infrastructure.index.EmbeddedSolrService;
 import se.streamsource.streamflow.web.infrastructure.index.SolrQueryService;
 import se.streamsource.streamflow.web.infrastructure.plugin.StreetAddressLookupConfiguration;
 import se.streamsource.streamflow.web.infrastructure.plugin.address.StreetAddressLookupService;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.zip.GZIPOutputStream;
+
+import static org.qi4j.api.util.Iterables.*;
+import static se.streamsource.streamflow.infrastructure.event.domain.source.helper.Events.*;
 
 /**
  * Implementation of Manager interface. All general JMX management methods
@@ -192,7 +192,7 @@ public interface ManagerComposite
             }
          };
 
-         stream.registerListener(failedLoginListener);
+         stream.registerListener( failedLoginListener );
       }
 
       public void stop() throws Exception
@@ -204,12 +204,18 @@ public interface ManagerComposite
 
       public void reindex() throws Exception
       {
+         DateTime startDateTime = new DateTime( );
+         logger.info( "Starting reindex at " + startDateTime.toString() );
+
+         logger.info( "Remove RDF index." );
          // Delete current index
          removeRdfRepository();
 
+         logger.info( "Remove Solr index." );
          // Remove Lucene index
          removeSolrLuceneIndex();
 
+         logger.info( "Reindexing ..." );
          // Reindex state
          reindexer.reindex();
 
@@ -217,8 +223,11 @@ public interface ManagerComposite
          StreetAddressLookupService streetLookup = (StreetAddressLookupService) module.serviceFinder().findService( StreetAddressLookupService.class ).get();
          if( streetLookup != null && ((StreetAddressLookupConfiguration)streetLookup.configuration()).enabled().get() )
          {
+            logger.info( "Reindexing StreetLookup." );
             streetLookup.reindex();
          }
+
+         logger.info( "Reindexing done in " + PeriodFormat.getDefault().print( new Duration( startDateTime, new DateTime( ) ).toPeriod() ) );
       }
 
       public String exportDatabase(boolean compress) throws IOException
@@ -299,7 +308,7 @@ public interface ManagerComposite
 
       public String exportEvents(@Name("Compress") boolean compress) throws IOException
       {
-         File exportFile = exportEvents0(compress);
+         File exportFile = exportEvents0( compress );
 
          return "Events exported to:" + exportFile.getAbsolutePath();
       }
@@ -329,10 +338,13 @@ public interface ManagerComposite
 
       public String backup() throws IOException, ParseException
       {
+         DateTime startDateTime = new DateTime( );
+         logger.info( "Started backup at " + startDateTime.toString() );
          String backupResult = backupEvents();
 
          backupResult += backupDatabase();
 
+         logger.info( "Backup done successfully in: " + PeriodFormat.getDefault().print( new Duration(startDateTime, new DateTime( ) ).toPeriod() ) );
          return backupResult;
       }
 
@@ -340,6 +352,8 @@ public interface ManagerComposite
       {
          try
          {
+            DateTime startDateTime = new DateTime(  );
+            logger.info( "Starting restore at " + startDateTime.toString() );
 
             // Restore data from latest backup in /backup
             File latestBackup = getLatestBackup();
@@ -349,15 +363,17 @@ public interface ManagerComposite
             {
                return "Error: no backup to restore";
             }
+            logger.info( "Fetching latest backup and start import database and reindex." );
 
-            importDatabase(latestBackup.getAbsolutePath());
+            // contains already a call to reindex
+            importDatabase( latestBackup.getAbsolutePath() );
 
-            // Reindex state
-            reindex();
+            logger.info( "Import database and reindex done. Clearing event database." );
 
             // Add events from backup files
             eventManagement.removeAll();
 
+            logger.info( "Replaying backup event files from time of snapshot backup." );
             File[] eventFiles = getBackupEventFiles();
 
             // Replay events from time of snapshot backup
@@ -393,6 +409,8 @@ public interface ManagerComposite
                if (ex[0] != null)
                   throw ex[0];
             }
+
+            logger.info( "Restore done successfully in: " + PeriodFormat.getDefault().print( new Duration(startDateTime, new DateTime( ) ).toPeriod() ) );
 
             return "Backup restored successfully";
          } catch (Exception ex)
@@ -537,7 +555,7 @@ public interface ManagerComposite
       public String databaseSize()
       {
          Transforms.Counter<EntityState> counter = new Transforms.Counter<EntityState>();
-         entityStore.get().entityStates(module).transferTo(Transforms.map(counter, Outputs.<EntityState>noop()));
+         entityStore.get().entityStates(module).transferTo( Transforms.map( counter, Outputs.<EntityState>noop() ) );
 
          return "Database contains " + counter.getCount() + " objects";
       }
