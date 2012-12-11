b52063f40016f98b91292ddb1af995e314fd87d4
Henrik Reinhold <henrik.reinhold@jayway.com>
2012-12-11 15:00:57 +0100

    Add missing value to assembler

--- streamflow-client-api/src/main/java/se/streamsource/streamflow/api/assembler/ClientAPIAssembler.java
+++ streamflow-client-api/src/main/java/se/streamsource/streamflow/api/assembler/ClientAPIAssembler.java
@@ -97,6 +97,7 @@ import se.streamsource.streamflow.api.workspace.cases.general.FormSignatureDTO;
 import se.streamsource.streamflow.api.workspace.cases.general.NoteDTO;
 import se.streamsource.streamflow.api.workspace.cases.general.PageSubmissionDTO;
 import se.streamsource.streamflow.api.workspace.cases.general.PermissionsDTO;
+import se.streamsource.streamflow.api.workspace.cases.general.SecondSigneeInfoValue;
 
 /**
  * Assembler for the Streamflow Client API.
@@ -142,7 +143,8 @@ public class ClientAPIAssembler
               FieldValueDTO.class,
               AttachmentFieldSubmission.class,
               AttachmentFieldDTO.class,
-              FormSignatureDTO.class
+              FormSignatureDTO.class,
+              SecondSigneeInfoValue.class
       );
 
       workspace.values(ContactAddressDTO.class,
