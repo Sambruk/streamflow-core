c4152999c8aeb01d2e501fb6f822cc66bd0ae7c0
Arvid Huss <arvid.huss@jayway.com>
2012-12-12 10:58:13 +0100

    STREAMFLOW-748 Stöd för dubbla signaturer i WebForms Flyttat DoubleSignatureTask skapande och eventuell mail skick ifrån SubmittedForms till SurfaceSummaryContext

--- web/src/main/java/se/streamsource/streamflow/web/domain/structure/form/SubmittedForms.java
+++ web/src/main/java/se/streamsource/streamflow/web/domain/structure/form/SubmittedForms.java
@@ -19,12 +19,9 @@ package se.streamsource.streamflow.web.domain.structure.form;
 import org.qi4j.api.common.ConstructionException;
 import org.qi4j.api.common.Optional;
 import org.qi4j.api.common.UseDefaults;
-import org.qi4j.api.concern.ConcernOf;
-import org.qi4j.api.concern.Concerns;
 import org.qi4j.api.entity.EntityReference;
 import org.qi4j.api.entity.Identity;
 import org.qi4j.api.entity.Queryable;
-import org.qi4j.api.injection.scope.Service;
 import org.qi4j.api.injection.scope.Structure;
 import org.qi4j.api.injection.scope.This;
 import org.qi4j.api.mixin.Mixins;
@@ -33,8 +30,6 @@ import org.qi4j.api.specification.Specification;
 import org.qi4j.api.structure.Module;
 import org.qi4j.api.util.Iterables;
 import org.qi4j.api.value.ValueBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import se.streamsource.streamflow.api.administration.form.AttachmentFieldValue;
 import se.streamsource.streamflow.api.administration.form.CommentFieldValue;
 import se.streamsource.streamflow.api.administration.form.RequiredSignatureValue;
@@ -44,31 +39,19 @@ import se.streamsource.streamflow.api.workspace.cases.general.FormDraftDTO;
 import se.streamsource.streamflow.api.workspace.cases.general.PageSubmissionDTO;
 import se.streamsource.streamflow.infrastructure.event.domain.DomainEvent;
 import se.streamsource.streamflow.util.Strings;
-import se.streamsource.streamflow.web.application.defaults.SystemDefaultsService;
-import se.streamsource.streamflow.web.application.mail.EmailValue;
-import se.streamsource.streamflow.web.context.surface.accesspoints.endusers.formdrafts.summary.SurfaceSummaryContext;
 import se.streamsource.streamflow.web.domain.entity.attachment.AttachmentEntity;
-import se.streamsource.streamflow.web.domain.interaction.gtd.CaseId;
 import se.streamsource.streamflow.web.domain.structure.SubmittedFieldValue;
 import se.streamsource.streamflow.web.domain.structure.attachment.FormAttachments;
-import se.streamsource.streamflow.web.domain.structure.caze.Case;
 import se.streamsource.streamflow.web.domain.structure.organization.AccessPoint;
-import se.streamsource.streamflow.web.domain.structure.task.DoubleSignatureTask;
-import se.streamsource.streamflow.web.domain.structure.task.DoubleSignatureTasks;
-import se.streamsource.streamflow.web.rest.service.mail.MailSenderService;
 
-import java.text.MessageFormat;
 import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
 
 import static se.streamsource.dci.api.RoleMap.*;
 
 /**
  * Maintains list of submitted forms on a case
  */
-@Concerns(SubmittedForms.DoubleSignatureTaskCreationConcern.class)
 @Mixins(SubmittedForms.Mixin.class)
 public interface SubmittedForms
 {
@@ -215,100 +198,6 @@ public interface SubmittedForms
       public boolean hasSubmittedForms()
       {
          return !state.submittedForms().get().isEmpty();
-      }
-
-   }
-
-   abstract class DoubleSignatureTaskCreationConcern
-      extends ConcernOf<SubmittedForms>
-      implements SubmittedForms
-   {
-      @Structure
-      Module module;
-
-      @This
-      CaseId.Data caseId;
-
-      @This
-      SubmittedForms forms;
-
-      @Optional
-      @Service
-      MailSenderService mailSender;
-
-      @Optional
-      @Service
-      SystemDefaultsService defaults;
-
-      final Logger logger = LoggerFactory.getLogger( SubmittedForms.class.getName() );
-
-      @This
-      FormDrafts submissions;
-
-      @This
-      DoubleSignatureTasks signatureTasks;
-
-
-      public SubmittedFormValue submitForm( FormDraft formSubmission, Submitter submitter )
-      {
-         SubmittedFormValue submittedForm = next.submitForm( formSubmission, submitter );
-
-         // Check for active signatures, catch and ignore IllegalArgumentException if we do not have a role AccessPoint
-         // in that case we are coming from the clients form wizard!!
-         AccessPoint accessPoint = null;
-         try
-         {
-            accessPoint = role( AccessPoint.class );
-         } catch (IllegalArgumentException ia)
-         {
-            // do nothing - this approach is used to determine if we are coming from Surface Webforms or from client form wizard.
-         }
-         if (accessPoint != null)
-         {
-            RequiredSignatures.Data requiredSignatures = module.unitOfWorkFactory().currentUnitOfWork().get( RequiredSignatures.Data.class, ((Identity) accessPoint).identity().get() );
-            Iterable<RequiredSignatureValue> activeSignatures = Iterables.filter( new Specification<RequiredSignatureValue>()
-            {
-               public boolean satisfiedBy( RequiredSignatureValue signature )
-               {
-                  return signature.active().get();
-               }
-            }, requiredSignatures.requiredSignatures().get() );
-
-            // set second signee if we expect one
-            if (Iterables.count( activeSignatures ) > 1)
-            {
-               if (submittedForm.secondsignee().get() != null && !submittedForm.secondsignee().get().singlesignature().get())
-               {
-                  DoubleSignatureTask task = signatureTasks.createTask( role( Case.class ), submittedForm, null );
-
-                  Form secondForm = module.unitOfWorkFactory().currentUnitOfWork().get( Form.class, requiredSignatures.requiredSignatures().get().get( 1 ).formid().get() );
-
-                  FormDraft draft = submissions.createFormDraft( secondForm );
-                  task.updateFormDraft( draft );
-
-                  ResourceBundle bundle = ResourceBundle.getBundle( SurfaceSummaryContext.class.getName(), role( Locale.class ) );
-
-                  try
-                  {
-                     ValueBuilder<EmailValue> builder = module.valueBuilderFactory().newValueBuilder( EmailValue.class );
-
-                     builder.prototype().subject().set( bundle.getString( "signature_notification_subject" ) );
-                     builder.prototype().content().set( MessageFormat.format( bundle.getString( "signature_notification_body" ),
-                           caseId.caseId().get(), defaults.config().configuration().webFormsProxyUrl().get() + "/cases/" + caseId.caseId().get()
-                           + "/formdrafts/" + ((Identity) draft).identity().get() + "/index" ) );
-                     builder.prototype().contentType().set( "text/plain" );
-                     builder.prototype().to().set( submittedForm.secondsignee().get().email().get() );
-
-                     mailSender.sentEmail( builder.newInstance() );
-
-                  } catch (Throwable throwable)
-                  {
-                     logger.error( "Could not send mail", throwable );
-                  }
-               }
-            }
-         }
-         return submittedForm;
       }
    }
 }
