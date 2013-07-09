8fdf8ced8d74aa0bcb03e3f8b3a12522e403bffe
Gustaf Nilsson Kotte <gustaf.nk@gmail.com>
2013-07-09 15:31:58 +0200

    Cleanup

--- webclient/app/modules/controllers/main.js
+++ webclient/app/modules/controllers/main.js
@@ -18,9 +18,6 @@
 (function() {
   'use strict';
 
-  var createFormResponse = {"events":[{"identity":"0092cac0-2293-42ad-be16-87080cbae891-cf","by":"administrator","entity":"37ed3d41-3338-427e-8542-c8ac0e794c2d-2","entityType":"se.streamsource.streamflow.web.domain.entity.caze.CaseEntity","name":"createdFormDraft","on":"2013-07-08T13:14:03.914Z","parameters":"{\"param1\":\"0092cac0-2293-42ad-be16-87080cbae891-ce\"}","usecase":"create","version":"1.8.0.1"},{"identity":"0092cac0-2293-42ad-be16-87080cbae891-d0","by":"administrator","entity":"0092cac0-2293-42ad-be16-87080cbae891-ce","entityType":"se.streamsource.streamflow.web.domain.entity.form.FormDraftEntity","name":"changedFormDraft","on":"2013-07-08T13:14:03.914Z","parameters":"{\"param1\":\"{\\\"description\\\":\\\"Informatörstest\\\",\\\"enteredEmails\\\":null,\\\"form\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-53\\\",\\\"mailSelectionEnablement\\\":null,\\\"pages\\\":[{\\\"fields\\\":[{\\\"enabled\\\":true,\\\"field\\\":{\\\"datatypeUrl\\\":null,\\\"description\\\":\\\"Välj ett datum\\\",\\\"field\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-5d\\\",\\\"fieldId\\\":\\\"Date1\\\",\\\"fieldValue\\\":{\\\"_type\\\":\\\"se.streamsource.streamflow.api.administration.form.DateFieldValue\\\",\\\"date\\\":null},\\\"mandatory\\\":false,\\\"note\\\":\\\"\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true}},\\\"message\\\":null,\\\"value\\\":\\\"2013-07-09T00:00:00.000Z\\\"},{\\\"enabled\\\":true,\\\"field\\\":{\\\"datatypeUrl\\\":null,\\\"description\\\":\\\"Namn\\\",\\\"field\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-60\\\",\\\"fieldId\\\":\\\"Text2\\\",\\\"fieldValue\\\":{\\\"_type\\\":\\\"se.streamsource.streamflow.api.administration.form.TextFieldValue\\\",\\\"hint\\\":\\\"förnamn, efternamn\\\",\\\"mandatory\\\":false,\\\"regularExpression\\\":null,\\\"width\\\":30},\\\"mandatory\\\":true,\\\"note\\\":\\\"\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true}},\\\"message\\\":null,\\\"value\\\":\\\"Gustaf, Nilsson Kotte22\\\"}],\\\"page\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-57\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true},\\\"title\\\":\\\"1. Första sidan\\\"},{\\\"fields\\\":[{\\\"enabled\\\":true,\\\"field\\\":{\\\"datatypeUrl\\\":null,\\\"description\\\":\\\"Meddelande\\\",\\\"field\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-66\\\",\\\"fieldId\\\":\\\"TextArea1\\\",\\\"fieldValue\\\":{\\\"_type\\\":\\\"se.streamsource.streamflow.api.administration.form.TextAreaFieldValue\\\",\\\"cols\\\":30,\\\"rows\\\":5},\\\"mandatory\\\":false,\\\"note\\\":\\\"\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true}},\\\"message\\\":null,\\\"value\\\":\\\"Aoeaoeaoeaoeaoe\\\"},{\\\"enabled\\\":true,\\\"field\\\":{\\\"datatypeUrl\\\":null,\\\"description\\\":\\\"Välj nåt\\\",\\\"field\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-69\\\",\\\"fieldId\\\":\\\"ComboBox2\\\",\\\"fieldValue\\\":{\\\"_type\\\":\\\"se.streamsource.streamflow.api.administration.form.ComboBoxFieldValue\\\",\\\"values\\\":[\\\"Alt 1\\\",\\\"Alt 2\\\",\\\"Alt 3\\\"]},\\\"mandatory\\\":false,\\\"note\\\":\\\"\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true}},\\\"message\\\":null,\\\"value\\\":\\\"Alt 1\\\"}],\\\"page\\\":\\\"6f466f61-0c8b-4171-96bb-7e14b6045743-5a\\\",\\\"rule\\\":{\\\"condition\\\":\\\"anyof\\\",\\\"field\\\":\\\"\\\",\\\"values\\\":[],\\\"visibleWhen\\\":true},\\\"title\\\":\\\"2. Andra sidan\\\"}],\\\"secondsignee\\\":null,\\\"signatures\\\":[],\\\"visibilityrules\\\":null}\"}","usecase":"create","version":"1.8.0.1"}],"timestamp":1373289243960};
-  var formDraft = {"description":"Informatörstest","enteredEmails":null,"form":"6f466f61-0c8b-4171-96bb-7e14b6045743-53","mailSelectionEnablement":null,"pages":[{"fields":[{"enabled":true,"field":{"datatypeUrl":null,"description":"Välj ett datum","field":"6f466f61-0c8b-4171-96bb-7e14b6045743-5d","fieldId":"Date1","fieldValue":{"_type":"se.streamsource.streamflow.api.administration.form.DateFieldValue","date":null},"mandatory":false,"note":"","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true}},"message":null,"value":"2013-07-09T00:00:00.000Z"},{"enabled":true,"field":{"datatypeUrl":null,"description":"Namn","field":"6f466f61-0c8b-4171-96bb-7e14b6045743-60","fieldId":"Text2","fieldValue":{"_type":"se.streamsource.streamflow.api.administration.form.TextFieldValue","hint":"förnamn, efternamn","mandatory":false,"regularExpression":null,"width":30},"mandatory":true,"note":"","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true}},"message":null,"value":"Gustaf, Nilsson Kotte22"}],"page":"6f466f61-0c8b-4171-96bb-7e14b6045743-57","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true},"title":"1. Första sidan"},{"fields":[{"enabled":true,"field":{"datatypeUrl":null,"description":"Meddelande","field":"6f466f61-0c8b-4171-96bb-7e14b6045743-66","fieldId":"TextArea1","fieldValue":{"_type":"se.streamsource.streamflow.api.administration.form.TextAreaFieldValue","cols":30,"rows":5},"mandatory":false,"note":"","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true}},"message":null,"value":"Aoeaoeaoeaoeaoe"},{"enabled":true,"field":{"datatypeUrl":null,"description":"Välj nåt","field":"6f466f61-0c8b-4171-96bb-7e14b6045743-69","fieldId":"ComboBox2","fieldValue":{"_type":"se.streamsource.streamflow.api.administration.form.ComboBoxFieldValue","values":["Alt 1","Alt 2","Alt 3"]},"mandatory":false,"note":"","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true}},"message":null,"value":"Alt 1"}],"page":"6f466f61-0c8b-4171-96bb-7e14b6045743-5a","rule":{"condition":"anyof","field":"","values":[],"visibleWhen":true},"title":"2. Andra sidan"}],"secondsignee":null,"signatures":[],"visibilityrules":false};
-
   var main = angular.module('sf.controllers', ['sf.services.project', 'sf.services.case']);
 
   function toggleToolbar($event) {
@@ -52,11 +49,7 @@
 
       caseService.createSelectedForm($params.projectId, $params.projectType, $params.caseId, formId).then(function(response){
         if (response.data.events.length === 0) {
-
           $scope.form = caseService.getFormDraftFromForm($params.projectId, $params.projectType, $params.caseId, formId)
-
-          // TODO!!! Use more general version!!!
-          //$scope.form = caseService.getFormDraftDebug($params.projectId, $params.projectType, $params.caseId)
         }
         else {
           var draftId = JSON.parse(response.data.events[0].parameters).param1;
@@ -65,16 +58,9 @@
       });
     }
 
-    $scope.fooTest = function(){
-      $scope.form = caseService.getFormDraftDebug($params.projectId, $params.projectType, $params.caseId);
-    }
-
     $scope.selectFormPage = function(page){
       $scope.currentFormPage = page;
     }
-
-    window.form = formDraft;
-    //$scope.form = formDraft;
   }]);
 
 
--- webclient/app/modules/services/case-service.js
+++ webclient/app/modules/services/case-service.js
@@ -133,26 +133,7 @@
           });
           }
         });
-      },
-
-      // For development only!!! API needs to be fixed to support more general case
-      getFormDraftDebug: function(projectId, projectType, caseId){
-        return $http({
-          method: 'GET',
-          url: 'https://test.sf.streamsource.se/streamflow/workspacev2/cases/37ed3d41-3338-427e-8542-c8ac0e794c2d-2/formdrafts/.json'
-        }).then(function(response){
-          return backendService.get({
-            specs:caseBase(projectId, projectType, caseId).concat([
-              {resources: 'formdrafts'},
-              {'index.links': response.data.index.links[0].id}
-              ]),
-            onSuccess:function (resource, result) {
-              result.push(resource.response.index);
-            }
-          });
-        });
       }
-
     }
   }]);
 
