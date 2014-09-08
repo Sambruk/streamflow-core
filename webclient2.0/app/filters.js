f38fd6fd446d2cd8e2cc38cac640a244eefac92a
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2014-09-08 11:28:55 +0200

    update: added directive to body again for style

--- webclient2.0/app/filters.js
+++ webclient2.0/app/filters.js
@@ -91,4 +91,21 @@ angular.module('sf')
 
           return matchingItems;
       }
+  })
+  .filter('truncate', function () {
+    return function (text, length, end) {
+      if (isNaN(length))
+        length = 10;
+
+      if (end === undefined)
+        end = "...";
+
+      if (text.length <= length || text.length - end.length <= length) {
+        return text;
+      }
+      else {
+        return String(text).substring(0, length-end.length) + end;
+      }
+
+    };
   });
