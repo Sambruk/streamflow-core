d6d6967a35b454ea2ba834c8bf5fb306e1a30aa6
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2014-09-16 15:10:03 +0200

    updated build.sh

--- webclient/build.sh
+++ webclient/build.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
+npm install
+bower install
+rm -rf bower_components/jquery
 gulp clean-build
 
