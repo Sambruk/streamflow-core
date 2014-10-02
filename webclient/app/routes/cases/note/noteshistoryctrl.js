15d64158d99b33caa8ed976b6dc5b63801e6fca6
Alexandra Törn Almö <alexandra.almo@jayway.com>
2014-10-02 14:10:29 +0200

    Show spinner when getting noteHistory.

--- webclient/app/routes/cases/note/noteshistoryctrl.js
+++ webclient/app/routes/cases/note/noteshistoryctrl.js
@@ -3,4 +3,13 @@ angular.module('sf')
   .controller('NotesHistoryCtrl', function($scope, caseService, $routeParams) {
     $scope.caseId = $routeParams.caseId;
     $scope.notesHistory = caseService.getAllNotes($routeParams.caseId);
-  });
+
+    $scope.showSpinner = {
+    	notesHistory: true
+    };
+
+    $scope.notesHistory.promise.then(function(){
+    	$scope.showSpinner.notesHistory = false;
+    });
+
+	});
