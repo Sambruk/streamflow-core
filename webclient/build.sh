b04053c2c02d95b72ff243b6877801bd5c45080a
Arvid Huss <arvid.huss@jayway.com>
2014-10-13 13:33:35 +0200

    Generated legal information file headers.

--- webclient/build.sh
+++ webclient/build.sh
@@ -14,8 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-npm install
-bower install
-rm -rf bower_components/jquery
-gulp clean-build --prod
 
