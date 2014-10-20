2cb0fa395521fd2cb77e85b300fa9f7662e4b445
Alexandra Törn Almö <alexandra.almo@jayway.com>
2014-10-20 15:56:33 +0200

    Fixed translate bug

--- webclient/app/components/breadcrumb-banner/breadcrumb-banner-directive.js
+++ webclient/app/components/breadcrumb-banner/breadcrumb-banner-directive.js
@@ -34,10 +34,7 @@ angular.module('sf')
       });
       
       $rootScope.$on('breadcrumb-updated', function(event, breadcrumbList) {
-        console.log('BREADCRUMB UPDATED');
-        console.log(breadcrumbList);
         var newbcItems = getBreadcrumbItems(breadcrumbList);
-        console.log(newbcItems);
         scope.breadcrumbList = getBreadcrumbItems(breadcrumbList);
       });
 
@@ -45,10 +42,7 @@ angular.module('sf')
         var bcList = [];
         _.each(breadcrumbList, function(breadcrumbItem){
           _.each(breadcrumbItem, function(val, key){
-            if(typeof val === 'string' && val !== undefined){
-              val = val.charAt(0).toUpperCase() + val.slice(1);
-              bcList.push(val);
-            }
+            bcList.push(val);
           });
         });
         return bcList;
