a1de5f6a50fe21036bebc6567a150649aa392ccb
Fjodor Ekström <fjodor.ekstrom@jayway.com>
2015-01-15 14:23:59 +0100

    update: fixed mobile styling

---
+++ webclient/app/routes/logout/logout-ctrl.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('sf')
+.controller('LogoutCtrl', function($scope, $location, $http, buildMode, navigationService){
+	$scope.logout = function(){
+    var urlValue;
+    if(buildMode == 'dev'){
+      urlValue = 'https://dummyuser:dummypass@test-sf.jayway.com/streamflow/';
+    } else {
+      urlValue = $location.$$protocol + '://dummyuser:dummypass@' + $location.$$host + ':' + $location.$$port + '/webclient/api';
+    }
+    $http.get(urlValue).error(function(res){
+      navigationService.linkTo('#/');
+      location.reload();
+    });
+  }
+
+  $scope.logout();
+});
