ac7c93cdeb97e5a2ceb4276e9634bc1b988c01ae
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-01-20 11:02:23 +0100

    Add missing use stricts

--- webclient/app/infrastructure/webform-rules-service.js
+++ webclient/app/infrastructure/webform-rules-service.js
@@ -14,6 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+'use strict';
+
 angular.module('sf')
 .factory('webformRulesService', function(){
 // TODO: Test all possible form configurations.
