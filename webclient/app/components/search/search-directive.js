e2497fd605da7042f9fafbf02c31a11faf2814fc
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-01-22 14:32:00 +0100

    Focus search field after selecting a search term

--- webclient/app/components/search/search-directive.js
+++ webclient/app/components/search/search-directive.js
@@ -4,7 +4,8 @@ angular.module('sf').directive('search', function ($location, navigationService)
   return {
     restrict: 'E',
     templateUrl: 'components/search/search.html',
-    link: function (scope) {
+    link: function (scope, element) {
+      // Translation map. Should use i18n for this.
       scope.searchTerms = {
         'createdOn': 'skapad',
         'caseType': 'ärendetyp',
@@ -24,9 +25,20 @@ angular.module('sf').directive('search', function ($location, navigationService)
       };
 
       scope.addSearchTermToQuery = function (event) {
+        // Get key from search term value.
         var searchTerm = _.invert(scope.searchTerms)[event.currentTarget.innerHTML] + ':';
+
+        // Don't let query be undefined.
         scope.query = scope.query || '';
+
+        // Add a whitespace to seperate from previous search value.
         scope.query += (scope.query.length === 0) ? searchTerm : ' ' + searchTerm;
+
+        // Defer focus of text field to next cycle to avoid problems with
+        // $apply. There must be a better way to do this?
+        _.defer(function () {
+          $(element).find('#main-searchtext').focus();
+        });
       };
 
       scope.search = function (query) {
