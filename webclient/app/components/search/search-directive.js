c138094fe52aa01892441d0f6d2cb4d6db90f1bf
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-01-22 13:48:15 +0100

    Add search terms

--- webclient/app/components/search/search-directive.js
+++ webclient/app/components/search/search-directive.js
@@ -5,6 +5,30 @@ angular.module('sf').directive('search', function ($location, navigationService)
     restrict: 'E',
     templateUrl: 'components/search/search.html',
     link: function (scope) {
+      scope.searchTerms = {
+        'createdOn': 'skapad',
+        'caseType': 'ärendetyp',
+        'project': 'projekt',
+        'label': 'etikett',
+        'assignedTo': 'tilldelad',
+        'name': 'namn',
+        'contactId': 'kontakdid',
+        'phoneNumber': 'telefon',
+        'emailAddress': 'email',
+        'today': 'idag',
+        'yesterday': 'igår',
+        'hour': 'timme',
+        'week': 'vecka',
+        'me': 'mig',
+        'createdBy': 'skapadav'
+      };
+
+      scope.addSearchTermToQuery = function (event) {
+        var searchTerm = _.invert(scope.searchTerms)[event.currentTarget.innerHTML] + ':';
+        scope.query = scope.query || '';
+        scope.query += (scope.query.length === 0) ? searchTerm : ' ' + searchTerm;
+      };
+
       scope.search = function (query) {
         $location.search({'query': query});
         navigationService.linkTo('/search');
