930d7201cfd47424acacf434857928719c9ebf35
Joacim Löwgren <joacim.lowgren@gmail.com>
2015-02-19 14:27:14 +0100

    Remove webclient from core repository and move it to its own
    The repository is now located at:
      git@github.com:jayway/streamflow-webclient.git

--- webclient/app/routes/login/login-ctrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('sf')
-.controller('LoginCtrl', function($scope){
-	
-});
--- webclient/app/routes/logout/logout-ctrl.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('sf')
-  .controller('LogoutCtrl', function($rootScope, $route){
-    $rootScope.$broadcast('logout', true);
-  });
