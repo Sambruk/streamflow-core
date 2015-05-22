84b44ce79f76260fdc0d88e3ca5b082110905595
Jens Nordahl <jens.nordahl@jayway.com>
2015-05-22 15:23:32 +0200

    Factor out building a case query from a TableQuery (wip)

---
+++ web/src/main/java/se/streamsource/streamflow/web/context/util/TableQueryConverter.java
@@ -0,0 +1,107 @@
+/**
+ *
+ * Copyright 2009-2014 Jayway Products AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.streamsource.streamflow.web.context.util;
+
+import static org.qi4j.api.query.QueryExpressions.orderBy;
+import static org.qi4j.api.query.QueryExpressions.templateFor;
+
+import org.qi4j.api.injection.scope.Service;
+import org.qi4j.api.injection.scope.Structure;
+import org.qi4j.api.injection.scope.Uses;
+import org.qi4j.api.query.Query;
+import org.qi4j.api.query.QueryExpressions;
+import org.qi4j.api.query.grammar.OrderBy;
+import org.qi4j.api.query.grammar.OrderBy.Order;
+import org.qi4j.api.structure.Module;
+
+import se.streamsource.dci.value.table.TableQuery;
+import se.streamsource.streamflow.web.application.defaults.SystemDefaultsService;
+import se.streamsource.streamflow.web.domain.Describable;
+import se.streamsource.streamflow.web.domain.interaction.gtd.DueOn;
+import se.streamsource.streamflow.web.domain.interaction.gtd.Status;
+import se.streamsource.streamflow.web.domain.structure.caze.Case;
+import se.streamsource.streamflow.web.domain.structure.caze.CasePriority;
+import se.streamsource.streamflow.web.domain.structure.created.CreatedOn;
+import se.streamsource.streamflow.web.domain.structure.organization.PrioritySettings;
+
+/** Converter for converting a Query<Case> using a TableQuery
+ */
+public class TableQueryConverter {
+
+   private TableQuery tableQuery;
+
+   @Structure
+   Module module;
+
+   @Service
+   SystemDefaultsService systemConfig;
+
+   public TableQueryConverter(@Uses TableQuery tableQuery) {
+      this.tableQuery = tableQuery;
+   }
+
+   public Query<Case> convert(Query<Case> originalQuery) {
+
+      Query<Case> caseQuery = module.queryBuilderFactory().newQueryBuilder( Case.class ).newQuery( originalQuery )
+            .orderBy( orderBy( templateFor( CreatedOn.class ).createdOn(), OrderBy.Order.DESCENDING ) );
+
+      if( systemConfig.config().configuration().sortOrderAscending().get())
+      {
+         caseQuery.orderBy( orderBy( templateFor(CreatedOn.class).createdOn(), OrderBy.Order.ASCENDING) );
+      }
+
+      // Paging
+      if (tableQuery.offset() != null)
+         caseQuery.firstResult(Integer.parseInt(tableQuery.offset()));
+      if (tableQuery.limit() != null)
+         caseQuery.maxResults(Integer.parseInt(tableQuery.limit()));
+      if (tableQuery.orderBy() != null)
+      {
+         String[] orderByValue = tableQuery.orderBy().split(" ");
+         Order order = orderByValue[1].equals("asc") ? Order.ASCENDING : Order.DESCENDING;
+
+         if (tableQuery.orderBy().equals("status"))
+         {
+            caseQuery.orderBy(QueryExpressions.orderBy(QueryExpressions.templateFor(Status.Data.class).status(), order));
+         } else if (orderByValue[0].equals("description"))
+         {
+            caseQuery.orderBy(QueryExpressions.orderBy(QueryExpressions.templateFor(Describable.Data.class).description(), order));
+         } else if (orderByValue[0].equals("dueOn"))
+         {
+            caseQuery.orderBy(QueryExpressions.orderBy(QueryExpressions.templateFor(DueOn.Data.class).dueOn(), order));
+         } else if (orderByValue[0].equals("createdOn"))
+         {
+            caseQuery.orderBy(QueryExpressions.orderBy(QueryExpressions.templateFor(CreatedOn.class).createdOn(), order));
+         }else if( orderByValue[0].equals( "priority" ))
+         {
+            caseQuery.orderBy( QueryExpressions.orderBy(
+                  QueryExpressions.templateFor( PrioritySettings.Data.class, QueryExpressions.templateFor( CasePriority.Data.class ).casepriority().get() ).priority(), revertSortOrder( order ) ) );
+         }
+      }
+
+      return caseQuery;
+   }
+
+   public static OrderBy.Order revertSortOrder( OrderBy.Order order )
+   {
+      if( OrderBy.Order.ASCENDING.equals( order ))
+         return OrderBy.Order.DESCENDING;
+      else
+         return OrderBy.Order.ASCENDING;
+   }
+
+}
