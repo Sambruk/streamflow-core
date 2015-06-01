17c331fcba895e85e7d50a6e8e0feaf8d02311d8
Jens Nordahl <jens.nordahl@jayway.com>
2015-06-01 10:27:00 +0200

    Respond 422 at query syntax errors

--- dci/src/main/java/se/streamsource/dci/value/table/gdq/GdQueryParseException.java
+++ dci/src/main/java/se/streamsource/dci/value/table/gdq/GdQueryParseException.java
@@ -16,7 +16,7 @@
  */
 package se.streamsource.dci.value.table.gdq;
 
-public class GdQueryParseException extends RuntimeException {
+public class GdQueryParseException extends IllegalArgumentException {
 
    private static final long serialVersionUID = 1L;
 
@@ -33,10 +33,5 @@ public class GdQueryParseException extends RuntimeException {
 
    public GdQueryParseException(String message, Throwable cause) {
       super(message, cause);
-   }
-
-   public GdQueryParseException(String message, Throwable cause,
-         boolean enableSuppression, boolean writableStackTrace) {
-      super(message, cause, enableSuppression, writableStackTrace);
    }
 }
