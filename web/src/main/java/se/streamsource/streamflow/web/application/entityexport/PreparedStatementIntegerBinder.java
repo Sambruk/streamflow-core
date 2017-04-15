7214ec4e6d2efecd6ebc4b2530a6270000e8927b
RuslanPopenko <ruslanpopenko@ukr.net>
2017-04-15 12:43:38 +0300

    Added license terms.

--- web/src/main/java/se/streamsource/streamflow/web/application/entityexport/PreparedStatementIntegerBinder.java
+++ web/src/main/java/se/streamsource/streamflow/web/application/entityexport/PreparedStatementIntegerBinder.java
@@ -1,3 +1,21 @@
+/**
+ *
+ * Copyright
+ * 2009-2015 Jayway Products AB
+ * 2016-2017 Föreningen Sambruk
+ *
+ * Licensed under AGPL, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.gnu.org/licenses/agpl.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package se.streamsource.streamflow.web.application.entityexport;
 
 import java.sql.PreparedStatement;
