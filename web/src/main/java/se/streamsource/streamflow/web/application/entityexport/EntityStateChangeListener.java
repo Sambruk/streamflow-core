f4cea6df41f81afcd02df43657f62be55679f8ab
RuslanPopenko <ruslanpopenko@ukr.net>
2017-07-03 13:43:38 +0300

    Updated license info

--- web/src/main/java/se/streamsource/streamflow/web/application/entityexport/EntityStateChangeListener.java
+++ web/src/main/java/se/streamsource/streamflow/web/application/entityexport/EntityStateChangeListener.java
@@ -1,14 +1,15 @@
 /**
+ *
  * Copyright
  * 2009-2015 Jayway Products AB
  * 2016-2017 Föreningen Sambruk
- * <p>
+ *
  * Licensed under AGPL, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.gnu.org/licenses/agpl.txt
- * <p>
+ *
+ *     http://www.gnu.org/licenses/agpl.txt
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
