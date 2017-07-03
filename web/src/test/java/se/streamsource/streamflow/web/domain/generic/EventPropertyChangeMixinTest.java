4559fcaa0d2c570912ffe218a6cf612c3ab2c7db
RuslanPopenko <ruslanpopenko@ukr.net>
2017-07-03 14:07:35 +0300

    Uncomment EventPropertyChangeMixinTest.

--- web/src/test/java/se/streamsource/streamflow/web/domain/generic/EventPropertyChangeMixinTest.java
+++ web/src/test/java/se/streamsource/streamflow/web/domain/generic/EventPropertyChangeMixinTest.java
@@ -58,7 +58,7 @@ public class EventPropertyChangeMixinTest
       new EntityTestAssembler().assemble( module );
    }
 
-   //@Test
+   @Test
    public void testEventMixin()
    {
       UnitOfWork uow = unitOfWorkFactory.newUnitOfWork();
