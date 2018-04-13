0bde5e1e5a3e92a08e2034cbb85e9e1b2a704815
3emluk <3emluk@gmail.com>
2018-04-13 10:56:16 +0300

    Fix obtain properties during exportJob if they are empty

--- web/src/main/java/se/streamsource/streamflow/web/application/entityexport/EntityExportJob.java
+++ web/src/main/java/se/streamsource/streamflow/web/application/entityexport/EntityExportJob.java
@@ -99,8 +99,12 @@ public class EntityExportJob implements Runnable
                final EntityDescriptor entityDescriptor = module.entityDescriptor( type );
                final EntityType entityType = entityDescriptor.entityType();
 
-               final JSONObject propertiesHolder = entity.getJSONObject(JSONEntityState.JSON_KEY_PROPERTIES);
-
+               final JSONObject propertiesHolder;
+               if (entity.has(JSONEntityState.JSON_KEY_PROPERTIES)){
+                   propertiesHolder = entity.getJSONObject(JSONEntityState.JSON_KEY_PROPERTIES);
+               } else {
+                   propertiesHolder = new JSONObject();
+               }
                final Iterable<PropertyType> existsProperties =
                        getNotNullProperties(propertiesHolder, entityType.properties() );
                final Iterable<AssociationType> existsAssociations =
