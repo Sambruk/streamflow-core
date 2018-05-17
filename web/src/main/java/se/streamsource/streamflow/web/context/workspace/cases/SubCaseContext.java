74aeccfffd339310223e04466c3e3f876e376a5c
3emluk <3emluk@gmail.com>
2018-05-17 10:29:13 +0300

    Return to usage case search result

--- web/src/main/java/se/streamsource/streamflow/web/context/workspace/cases/SubCaseContext.java
+++ web/src/main/java/se/streamsource/streamflow/web/context/workspace/cases/SubCaseContext.java
@@ -3,16 +3,19 @@ package se.streamsource.streamflow.web.context.workspace.cases;
 import se.streamsource.dci.api.IndexContext;
 import se.streamsource.dci.api.RoleMap;
 import se.streamsource.streamflow.web.context.RequiresPermission;
+import se.streamsource.streamflow.web.context.workspace.CaseSearchResult;
 import se.streamsource.streamflow.web.domain.interaction.security.PermissionType;
 import se.streamsource.streamflow.web.domain.structure.caze.Case;
 import se.streamsource.streamflow.web.domain.structure.caze.SubCase;
 
+import java.util.Collections;
+
 /**
  * Created by dmizem from Ubrainians for imCode on 16.05.18.
  */
-public class SubCaseContext implements IndexContext<Case> {
-    public Case index() {
-        return RoleMap.role(SubCase.Data.class).parent().get();
+public class SubCaseContext implements IndexContext<CaseSearchResult> {
+    public CaseSearchResult index() {
+        return new CaseSearchResult(Collections.singleton(RoleMap.role(SubCase.Data.class).parent().get()));
     }
 
     @RequiresPermission(PermissionType.write)
--- web/src/main/java/se/streamsource/streamflow/web/context/workspace/cases/SubCasesContext.java
+++ web/src/main/java/se/streamsource/streamflow/web/context/workspace/cases/SubCasesContext.java
@@ -3,22 +3,21 @@ package se.streamsource.streamflow.web.context.workspace.cases;
 import se.streamsource.dci.api.IndexContext;
 import se.streamsource.dci.api.RoleMap;
 import se.streamsource.streamflow.web.context.RequiresPermission;
+import se.streamsource.streamflow.web.context.workspace.CaseSearchResult;
 import se.streamsource.streamflow.web.domain.interaction.gtd.RequiresStatus;
 import se.streamsource.streamflow.web.domain.interaction.security.PermissionType;
 import se.streamsource.streamflow.web.domain.structure.caze.Case;
 import se.streamsource.streamflow.web.domain.structure.caze.SubCases;
-
-import java.util.List;
 
 import static se.streamsource.streamflow.api.workspace.cases.CaseStates.OPEN;
 
 /**
  * Created by dmizem from Ubrainians for imCode on 15.05.18.
  */
-public class SubCasesContext implements IndexContext<Iterable<Case>> {
+public class SubCasesContext implements IndexContext<CaseSearchResult> {
 
-    public List<Case> index() {
-        return RoleMap.role(SubCases.Data.class).subCases().toList();
+    public CaseSearchResult index() {
+        return new CaseSearchResult(RoleMap.role(SubCases.Data.class).subCases().toList());
     }
 
     @RequiresStatus(OPEN)
